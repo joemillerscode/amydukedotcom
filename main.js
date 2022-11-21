7bf8658606907b96dd6aeb6b55eb0c6819f5c3bd
github username <Joemiller065@gmail.com>
2022-11-21 09:18:09 -0500

    photo-wll inspo

--- main.js
+++ main.js
@@ -2,8 +2,4 @@ import './style.css'
 // import javascriptLogo from './img/2time.jpeg'
 // import { setupCounter } from './counter.js'
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <p>AmyDuke.com coming soon </p>
-//   </div>
-// `
+const photoWall = document.querySelector('#photo-wall');

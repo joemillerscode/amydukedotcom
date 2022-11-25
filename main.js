4c1470945937a9b70b57d58ea2faf7e852da7761
github username <Joemiller065@gmail.com>
2022-11-25 10:21:38 -0500

    sass in, gallery first look in

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-import './style.css'
+import './style.scss'
 // import javascriptLogo from './img/2time.jpeg'
 // import { setupCounter } from './counter.js'
 
-const photoWall = document.querySelector('#photo-wall');
+// const photoWall = document.querySelector('#photo-wall');
+
+// photoWall.addEventListener('wheel', (event) => {
+//   event.preventDefault();
+
+//   photoWall.scrollBy({
+//     left: event.deltaY < 0 ? -30 : 30,
+    
+//   });
+// });
+
+//horizontal scroll with mousewheel above.

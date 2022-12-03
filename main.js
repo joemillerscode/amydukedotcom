430b39ff07a10581d7aa2eaf0e1e98973deae9ee
github username <Joemiller065@gmail.com>
2022-12-03 13:27:37 -0500

    all photos load at the same time, some disposable nav work we prob wont use

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import './style.scss'
 const images = import.meta.glob('./img/*.jpg')
-const  galleryTiles = document.querySelectorAll('.test')
 
 const srcArr = []
 for (const path in images) {
@@ -8,26 +7,39 @@ for (const path in images) {
 }
 
 function shuffleArray(array) {
-  console.log('shug')
+  console.log('images shuffled')
   for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
 }
 
-const galleryWrapper = document.getElementById('gallery')
-
+function addImagesToDom(imgArr) {
+  const galleryWrapper = document.getElementById('gallery')
+  imgArr.map(image => {
+    const imgContainer = document.createElement('div')
+    imgContainer.className = "img-container"
+    imgContainer.appendChild(image)
+    galleryWrapper.appendChild(imgContainer)
+  })
+}
+  
+  
+  // --------------------------------------------------------------
 shuffleArray(srcArr)
 
+let imgCounter = 0
+const loadedImgs = []
+
 for (let i = 0; i < srcArr.length; i++){
-
-    const photoDiv = document.createElement('div')
-    photoDiv.className = "img-container"
-
-    const galleryImg = document.createElement('img')
+  const galleryImg = new Image()
+    galleryImg.className = "gallery-img"
     galleryImg.src = srcArr[i]
+    galleryImg.onload = (e) =>  {
+      loadedImgs.push(galleryImg)
+      if (loadedImgs.length === srcArr.length){
+        addImagesToDom(loadedImgs)
+      }
+    }
 
-    photoDiv.appendChild(galleryImg)
-
-    galleryWrapper.appendChild(photoDiv)
 }

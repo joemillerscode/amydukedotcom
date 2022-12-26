63ac53d989eb57e0f4e5b31115cb6301962452bf
mrjoemiller <joe@joemiller.co>
2022-12-26 11:27:25 -0500

    ANOTHER RESTART

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 import './style.scss'
 const images = import.meta.glob('./img/*.jpg')
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
-// TODO: Add SDKs for Firebase products that you want to use
+// // TODO: Add SDKs for Firebase products that you want to use
 
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDxnO5X4y9ZjRCK4aMq28kd0gtPpS_TwF4",
-  authDomain: "amydukedotcom.firebaseapp.com",
-  databaseURL: "https://amydukedotcom-default-rtdb.firebaseio.com",
-  projectId: "amydukedotcom",
-  storageBucket: "amydukedotcom.appspot.com",
-  messagingSenderId: "460620932850",
-  appId: "1:460620932850:web:4d0cadbe7fe06b7f7bd26b",
-  measurementId: "G-Q6Q39261CP"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDxnO5X4y9ZjRCK4aMq28kd0gtPpS_TwF4",
+//   authDomain: "amydukedotcom.firebaseapp.com",
+//   databaseURL: "https://amydukedotcom-default-rtdb.firebaseio.com",
+//   projectId: "amydukedotcom",
+//   storageBucket: "amydukedotcom.appspot.com",
+//   messagingSenderId: "460620932850",
+//   appId: "1:460620932850:web:4d0cadbe7fe06b7f7bd26b",
+//   measurementId: "G-Q6Q39261CP"
+// };
 
-// Initialize Firebase
+// // Initialize Firebase
 
-const app = initializeApp(firebaseConfig);
-
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);

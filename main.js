37017dfb1da830571a1b1b606706492161ceda73
mrjoemiller <joe@joemiller.co>
2023-07-07 12:29:28 -0400

    looking good on the smallest screens

--- main.js
+++ main.js
@@ -1,27 +1,13 @@
-import './styles.scss'
-const images = import.meta.glob('./img/*.jpg')
+// Description: Main JS file for the website
+// const images = import.meta.glob('./img/*.jpg')
 
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.menu');
+  const hamburger = document.querySelector('.hamburger');
 
-// // TODO: Add SDKs for Firebase products that you want to use
-
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDxnO5X4y9ZjRCK4aMq28kd0gtPpS_TwF4",
-//   authDomain: "amydukedotcom.firebaseapp.com",
-//   databaseURL: "https://amydukedotcom-default-rtdb.firebaseio.com",
-//   projectId: "amydukedotcom",
-//   storageBucket: "amydukedotcom.appspot.com",
-//   messagingSenderId: "460620932850",
-//   appId: "1:460620932850:web:4d0cadbe7fe06b7f7bd26b",
-//   measurementId: "G-Q6Q39261CP"
-// };
-
-// // Initialize Firebase
-
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+  menuToggle.addEventListener('click', function() {
+    menu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+  });
+});

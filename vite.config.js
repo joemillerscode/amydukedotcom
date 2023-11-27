514a9d157bbe54eeb4b50e38de5c1c72fd7b29d5
mrjoemiller <joe@joemiller.co>
2023-11-27 12:46:12 -0500

    vite multi page

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    build: {
-        emptyOutDir: true,
-        rollupOptions: {
-            output: {
-                entryFileNames: "[name].[hash].js",
-                chunkFileNames: "[name].[hash].js",
-                assetFileNames: "img/[name].[ext]"
-            }
-        }
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about/index.html'
+        // Add more pages as needed
+      },
+      output: {
+        entryFileNames: "[name].[hash].js",
+        chunkFileNames: "[name].[hash].js",
+        assetFileNames: "img/[name].[ext]"
+      }
     }
+  }
 })
